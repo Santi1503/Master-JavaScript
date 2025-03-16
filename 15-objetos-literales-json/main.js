8494f41eeb9c2ac48bcc07af2d53fd6a2586f728
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2025-03-16 14:47:40 +0100

    FIX

---
+++ 15-objetos-literales-json/main.js
@@ -0,0 +1,14 @@
+// objetos literales
+
+let pelicula = {
+    titulo: 'Batman vs Superman',
+    year: 2017,
+    pais: 'EEUU',
+    genero: 'Acción',
+    director: 'Zack Snyder',
+}
+
+console.log(pelicula.titulo);
+
+pelicula.pais = 'Colombia';
+console.log(pelicula);
